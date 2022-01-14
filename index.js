64179d3a12810298aff3f912de7a7a12ef0a90d0
Tejas Ashok <73535431+thesh4de@users.noreply.github.com>
2022-01-14 22:33:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js')
 const bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"]});
 
-const token  = 'OTMxNDUyODk1MjY0NjYxNTk0.YeEpGw.35rhfZUJREbuhllu5g8OeteFQr0';
+const token  = '';
 
 bot.on('ready', () =>{
     console.log('This bot is online!');
